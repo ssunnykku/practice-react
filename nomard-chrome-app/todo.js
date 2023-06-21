42a343c20c67395d364c54c1119c40db0e610f0a
ssunnykku <ssunnykku@gmail.com>
2023-06-21 20:35:20 +0900

    derete todo

--- nomard-chrome-app/todo.js
+++ nomard-chrome-app/todo.js
@@ -2,14 +2,37 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-function handleToDoSubmit(event) {
-  event.preventDefault();
+const toDos = [];
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  span.innerText = newTodo;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "X";
+  toDoList.addEventListener("click", deleteToDo);
   li.appendChild(span);
-  span.innerText = toDoInput.value;
-  toDoInput.value = "";
   toDoList.appendChild(li);
+  li.appendChild(deleteBtn);
+}
+
+// adding todo
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  // saving
+  // toDos.push(toDoInput.value);
+  paintToDo(toDoInput.value);
+  toDoInput.value = "";
+}
+
+// delete todo
+function handdleToDoDelete(event) {
+  event.preventDefault();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
