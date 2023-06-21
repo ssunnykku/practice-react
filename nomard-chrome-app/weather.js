0a78c01c00460789b4074623eba4b2727bce96fc
ssunnykku <ssunnykku@gmail.com>
2023-06-22 00:24:52 +0900

    weather

--- nomard-chrome-app/weather.js
+++ nomard-chrome-app/weather.js
@@ -4,9 +4,21 @@ function ok(position) {
   console.log("You live in", lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3801568f196051b33a8d2e6deaca50f3
 `;
-  console.log(url);
-}
+  fetch(url).then((res) =>
+    res.json().then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const temp = document.querySelector("#weather span:nth-child(2)");
+      const city = document.querySelector("#weather span:nth-child(3)");
+      const country = document.querySelector("#weather span:nth-child(4)");
 
+      weather.innerText = data.weather[0].main;
+      city.innerText = data.name;
+      country.innerText = data.sys.country;
+      c = String(data.main.temp - 273.15);
+      temp.innerText = c.substr(0, 4);
+    })
+  );
+}
 function error() {
   alert("Can't find you. No weather for you");
 }
