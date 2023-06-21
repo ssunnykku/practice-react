1ae2733a8f20073007498a43c69262a724d6fa24
ssunnykku <ssunnykku@gmail.com>
2023-06-21 16:14:13 +0900

    Setup

--- nomard-chrome-app/clock.js
+++ nomard-chrome-app/clock.js
@@ -1,28 +1,3 @@
-const images = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
-];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `image/${chosenImage}`;
-
-document.body.appendChild(bgImage);
-
-// 시계
-
 let clock = document.querySelector(".clock");
 
 function getTime() {
