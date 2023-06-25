3d0295a787875d0185d1b5f85f4511f8c9ea94f4
ssunnykku <ssunnykku@gmail.com>
2023-06-25 15:48:35 +0900

    react_ coin tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,36 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    console.log(toDos);
-  };
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  const [loading, setLoading] = useState(true);
+  const [coin, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
+      <div>
+        <h1>The Coins! ({coin.length})</h1>
+        {loading ? <strong>Loading...</strong> : null}
+      </div>
       <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+        {coin.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}: ${coin.quotes.USD.price})
+          </li>
         ))}
       </ul>
     </div>
