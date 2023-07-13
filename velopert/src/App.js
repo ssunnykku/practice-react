da1bf6f561fb427beca5dec49e27ed7ac12afcd9
ssunnykku <ssunnykku@gmail.com>
2023-07-13 15:10:51 +0900

    react_vlpt 13. 배열에 항목 추가하기

--- velopert/src/App.js
+++ velopert/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import InputSample from './InputSample';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -20,17 +36,35 @@ function App() {
       username: 'liz',
       email: 'liz@example.com',
     },
-  ];
+  ]);
 
   // useRef(여기) : 여기 값이 .current 값의 기본이 된다. .current 값을 수정 혹은 조회할 수 있다.
   const nextId = useRef(4);
 
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+    setInputs({
+      username: '',
+      email: '',
+    });
+
     nextId.current += 1;
   };
   return (
     <div>
       {/* <InputSample /> */}
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} />
     </div>
   );
